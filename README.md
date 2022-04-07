a71f0c9ff414c1961b8c315847717c927e6f95cd
chendjou237 <chenxhenor@gmail.com>
2022-04-07 19:06:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# heal_point
+ the only health care app that you will ever need trust me, it's mind blowing
---
+++ android/app/src/main/kotlin/com/example/heal_point/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.heal_point
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
