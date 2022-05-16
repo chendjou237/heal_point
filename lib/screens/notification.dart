a52959b6fe55f626bd20ab728bb2fb8a6dc716dc
PROSPER <prolyfickofficial@gmail.com>
2022-05-16 03:35:28 +0100

    sss

---
+++ lib/screens/notification.dart
@@ -0,0 +1,121 @@
+// TODO Implement this library.
+// ignore: unused_import
+import 'package:flutter/material.dart';
+
+class Notifications extends StatelessWidget {
+  const Notifications({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(),
+      body: listView(),
+    );
+  }
+
+  PreferredSizeWidget appBar() {
+    return AppBar(
+      title: Text('Notifications'),
+      backgroundColor: Color.fromARGB(255, 112, 171, 219),
+    );
+  }
+
+  Widget listView() {
+    backgroundColor: Color.fromARGB(255, 226, 250, 255);
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return listViewItem(index);
+        },
+        separatorBuilder: (context, index) {
+          return Divider(height: 0);
+        },
+        itemCount: 15);
+  }
+
+  Widget listViewItem(int index) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          prefixIcon(),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  message(index),
+                  timeAndDate(index),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget prefixIcon() {
+    return Container(
+      height: 50,
+      width: 50,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.grey.shade300,
+      ),
+      child: Icon(Icons.notifications, size:25, color: Color.fromARGB(255, 5, 25, 204),
+      ),
+    );
+  }
+
+  Widget message(int index) {
+    double textSize = 14;
+    return Container(
+      child: RichText(
+        maxLines: 3,
+        overflow: TextOverflow.ellipsis,
+        text: TextSpan(
+          text: 'Message',
+          style: TextStyle(
+              fontSize: textSize,
+              color: Colors.black,
+              fontWeight: FontWeight.bold),
+          children: [
+            TextSpan(
+              
+              text: 'Message Description',
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget timeAndDate(int index) {
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "15-05-2022",
+            style: TextStyle(
+              fontSize: 10,
+            ),
+          ),
+          Text(
+            "03:04 am ",
+            style: TextStyle(
+              fontSize: 10,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
