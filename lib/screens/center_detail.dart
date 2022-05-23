c7e9f8f44b0eecac3f9a6e53d075a9ce045f6f5a
Abdoul <azisabdoul75@gmail.com>
2022-05-23 18:14:56 +0100

    fire

--- lib/screens/center_detail.dart
+++ lib/screens/center_detail.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:heal_point/utilities/palette.dart';
+import 'package:pay_unit_sdk/pay_unit_sdk.dart';
 
 class CenterDetails extends StatelessWidget {
-  const CenterDetails({ Key? key }) : super(key: key);
+  const CenterDetails({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
